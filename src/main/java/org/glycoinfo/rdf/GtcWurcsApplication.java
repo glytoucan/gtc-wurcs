27fc26355610268e27455fc783a87082d5e7a4bb
daisuke shinmachi <sinmachi@soka.ac.jp>
2016-12-19 16:10:52 +0900

    first commit.

---
+++ src/main/java/org/glycoinfo/rdf/GtcWurcsApplication.java
@@ -0,0 +1,12 @@
+package org.glycoinfo.rdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GtcWurcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GtcWurcsApplication.class, args);
+	}
+}
