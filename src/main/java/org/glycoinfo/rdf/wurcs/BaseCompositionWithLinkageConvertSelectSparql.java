d51e890044b03c3be9916f9cb5a546db8d2ff255
aoki <aokinobu@gmail.com>
2019-01-18 19:52:34 +0900

    copy from gtcSparql

--- src/main/java/org/glycoinfo/rdf/wurcs/BaseCompositionWithLinkageConvertSelectSparql.java
+++ src/main/java/org/glycoinfo/rdf/wurcs/BaseCompositionWithLinkageConvertSelectSparql.java
@@ -1,7 +1,5 @@
 package org.glycoinfo.rdf.wurcs;
 
-import javax.annotation.Resource;
-
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
---
+++ src/main/java/org/glycoinfo/rdf/wurcs/WurcsMonosaccharide.java
@@ -0,0 +1,7 @@
+package org.glycoinfo.rdf.wurcs;
+
+import org.glycoinfo.rdf.glycan.Monosaccharide;
+
+public interface WurcsMonosaccharide extends Monosaccharide {
+	public static final String WurcsMonosaccharideURI = "WurcsMonosaccharideURI";
+}
